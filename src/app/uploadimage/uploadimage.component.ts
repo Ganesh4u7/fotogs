a92e14012eb9f660bcffbb7095b27c11f0d7b29a
Ganesh4u7 <42564733+Ganesh4u7@users.noreply.github.com>
2020-02-12 01:00:49 +0530

    Add files via upload

---
+++ src/app/uploadimage/uploadimage.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
+
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'uploadimage',
+  templateUrl: './uploadimage.component.html',
+  styleUrls: ['./uploadimage.component.css']
+})
+export class UploadimageComponent implements OnInit {
+
+  @Input() file: File;
+  @Input() category: String;
+
+  task: AngularFireUploadTask;
+
+  percentage: Observable<number>;
+  snapshot: Observable<any>;
+  downloadURL: string;
+
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore) { }
+
+  ngOnInit() {
+    this.startUpload();
+  }
+
+  startUpload() {
+
+    // The storage path
+    const path = `${this.category}/${Date.now()}_${this.file.name}`;
+
+    // Reference to storage bucket
+    const ref = this.storage.ref(path);
+
+    // The main task
+    this.task = this.storage.upload(path, this.file);
+
+    // Progress monitoring
+    this.percentage = this.task.percentageChanges();
+
+    this.snapshot   = this.task.snapshotChanges().pipe(
+      tap(console.log),
+      // The file's download URL
+      finalize( async() =>  {
+        this.downloadURL = await ref.getDownloadURL().toPromise();
+
+        // firebase.database().ref(`${this.category}` ).set(this.downloadURL);
+     this.db.collection(`${this.category}`).add( { downloadURL: this.downloadURL, path });
+      }),
+    );
+  }
+
+  isActive(snapshot) {
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes;
+  }
+
+}
